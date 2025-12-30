19aecd07cf2b80336d40709e41e9476dd81ffa04
syntax-elpadrino <eduardoeduuh@proton.me>
2025-12-30 23:10:16 +0300

    languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,15 @@
+// Write a script that prints 3 lines:
+
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+let c = "C is fun"
+let python = "Python is cool"
+let js = "JavaScript is amazing"
+
+console.log(c)
+console.log(python)
+console.log(js)
