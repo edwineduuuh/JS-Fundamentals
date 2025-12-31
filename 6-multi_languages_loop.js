92ceb2b84f8676a4fb70d735dd22d2f21df79828
syntax-elpadrino <eduardoeduuh@proton.me>
2025-12-31 12:37:20 +0300

    Loop to languages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,23 @@
+// Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+
+// The first line: “C is fun”
+// The second line: “Python is cool”
+// The third line: “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use any if/else statement
+// You can use only one console.log
+// You must use a loop (while, for, etc.)
+const strings = ["C is fun", "Python is cool", "JavaScript is amazing"]
+for (const string of strings) {
+    console.log(string)
+}
+
+// or
+
+let i = 0
+
+while (i < strings.length) {
+    console.log(strings[i])
+    i++
+}
